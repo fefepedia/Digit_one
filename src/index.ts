0cdc683f0d43a6c5bd633599940b59b40f90e9ad
Serban-Timofte-EBR <timoftesrbn@gmail.com>
2023-09-22 15:51:50 +0300

    CORS implementation

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,25 @@ import authDashboard from './routes/authDashboard';
 
 const app = express();
 
+const allowedOrigins = ['http://localhost3000/', 'http://localhost:3001/', 'http://localhost:3000/api/users/login', 'http://localhost:3001/login'];
+
 // Middleware
 app.use(bodyParser.json()); // Replaces the express.json() middleware for body parsing
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
+);
+
 
 // Environment variables setup
 dotenv.config();
