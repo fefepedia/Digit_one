97d069e280d972d4f08e6b97daa7bf04b177a9c2
fefepedia <visiness@lsbu.ac.uk>
2023-09-28 17:09:11 +0100

    Merg aproape toate

--- src/controllers/inventoryController.ts
+++ src/controllers/inventoryController.ts
@@ -2,17 +2,18 @@ import { Request, Response } from 'express';
 import Inventory, { IInventory } from '../models/Inventory';
 import InventoryItem, { IInventoryItem } from '../models/InventoryItem';
 import { inventorySanitizer } from '../helpers/sanitizers/inverntorySanitizer';
+import {Types} from 'mongoose';
+import mongoose from 'mongoose';
+import SuperInventory from '../models/SuperInventory';
+
 
 // Add new Inventory
 export const addInventory = async (req: Request, res: Response) => {
   const sanitizedInventoryObject = inventorySanitizer(req.body);
-
   const newInventory: IInventory = new Inventory(sanitizedInventoryObject);
   const result = await newInventory.save();
-
   return res.json(result).status(200);
 };
-
 
 export const getInventory = async (req: Request, res: Response) => {
   try {
@@ -21,13 +22,11 @@ export const getInventory = async (req: Request, res: Response) => {
     if (!inventory) {
       return res.status(404).json({ error: 'Inventory not found.' });
     }
-
     return res.status(200).json(inventory);
   } catch (error) {
     return res.status(500).json({ error: 'An error occurred while retrieving the inventory.' });
   }
 };
-
 
 export const updateInventory = async (req: Request, res: Response) => {
   try {
@@ -57,24 +56,36 @@ export const updateInventoryItem = async (req: Request, res: Response) => {
   }
 };
 
-
-
 // Add Inventory Item
 export const addInventoryItem = async (req: Request, res: Response) => {
   const newInventoryItem: IInventoryItem = new InventoryItem(req.body);
+
   const result = await newInventoryItem.save();
 
   return res.json(result);
 };
 
-
 export const removeInventoryItem = async (req: Request, res: Response) => {
   try {
-    const { id } = req.params; // Assuming the ID of the item to be removed is passed as a URL parameter.
-    const removedItem = await InventoryItem.findByIdAndRemove(id);
+    const { id } = req.params;
+    const removedItem = await InventoryItem.findOneAndDelete({ _id: id });
 
     if (!removedItem) {
       return res.status(404).json({ error: 'InventoryItem not found.' });
+    }
+    const inventory = await Inventory.findById(removedItem.inventoryId);
+
+    if (!inventory) {
+      return res.status(404).json({error: 'Inventory not found.'});
+    }
+
+    
+
+    await inventory.save();
+    const remainingItems = await InventoryItem.find({ inventoryId: removedItem.inventoryId });
+    if(remainingItems.length === 0) {
+      // Call a function to reflect this change in the SuperInventory
+      await removeFromSuperInventory(inventory._id);
     }
 
     return res.status(200).json({ message: 'Inventory item removed successfully.' });
@@ -83,22 +94,65 @@ export const removeInventoryItem = async (req: Request, res: Response) => {
   }
 };
 
+const removeFromSuperInventory = async (inventoryId: mongoose.Types.ObjectId) => {
+  try {
+    const superInventory = await SuperInventory.findOne({ inventories: inventoryId });
+    if (superInventory) {
+      superInventory.inventories = superInventory.inventories.filter(i => !i.equals(inventoryId));
+      await superInventory.save();
+    }
+  } catch (error) {
+    console.log('Error removing inventory from SuperInventory', error);
+  }
+};
 
+export const removeInventory = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const removedInventory = await Inventory.findByIdAndDelete(id);
+
+    if (!removedInventory) {
+      return res.status(404).json({ error: 'Inventory not found.' });
+    }
+
+    // Reflect this change in the SuperInventory
+    const objectId = new mongoose.Types.ObjectId(id);
+    await removeFromSuperInventory(objectId);
+
+    return res.status(200).json({ message: 'Inventory removed successfully.' });
+  } catch (error) {
+    return res.status(500).json({ error: 'An error occurred while removing the inventory.' });
+  }
+};
 
 
 
 export const getAllItemsForInventory = async (req: Request, res: Response) => {
+  console.log("Inside getAllItemsForInventory");
   try {
     const inventoryId = req.params.id;
 
+    // Debugging log:
+    console.log("Trying to fetch items for inventoryId:", inventoryId);
+
+    // Check if the inventory exists
     const inventory = await Inventory.findById(inventoryId);
 
     if (!inventory) {
+      console.log("Inventory not found for ID:", inventoryId);  // Debugging log
       return res.status(404).json({ error: 'Inventory not found.' });
     }
 
-    return res.status(200).json(inventory.items);
+    // Fetch the items for this inventory from the InventoryItem model
+    const items = await InventoryItem.find({ inventoryId: inventoryId });
+
+    console.log("Fetched items:", items);  // Debugging log
+
+    return res.status(200).json(items);
   } catch (error) {
+    console.error("Error while fetching items:", error);  // Debugging log
     return res.status(500).json({ error: 'An error occurred while fetching the items.' });
   }
 };
+
+
--- src/routes/inventoryRoutes.ts
+++ src/routes/inventoryRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addInventory, addInventoryItem, getInventory, removeInventoryItem, updateInventory, updateInventoryItem, getAllItemsForInventory } from '../controllers/inventoryController';
+import { removeInventory, addInventory, addInventoryItem, getInventory, removeInventoryItem, updateInventory, updateInventoryItem, getAllItemsForInventory } from '../controllers/inventoryController';
 
 const router = express.Router();
 
@@ -7,11 +7,12 @@ router.post('/add-inventory', addInventory);
 router.put('/update-inventory/:id', updateInventory);
 router.put('/update-inventory-item/:id', updateInventoryItem);
 //router.patch('/patch-inventory');
-//router.delete('/delete-inventory/:id');
+router.delete('/remove-inventory/:id', removeInventory);
 //router.get('/inventories');
+router.get('/inventory/:id/items', getAllItemsForInventory);
 router.get('/inventory/:id',getInventory);
 router.delete('/remove-inventory-item/:id', removeInventoryItem);
 router.post('/add-inventory-item', addInventoryItem);
-router.get('/inventory/:id/items', getAllItemsForInventory);
+
 
 export default router;
